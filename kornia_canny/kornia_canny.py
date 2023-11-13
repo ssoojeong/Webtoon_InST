84d92e5eb2cd416a7c72007cee1a953f65d8f6d7
ssoojeong <104249792+ssoojeong@users.noreply.github.com>
2023-11-13 12:51:46 +0900

    Update kornia_canny.py

--- kornia_canny/kornia_canny.py
+++ kornia_canny/kornia_canny.py
@@ -10,10 +10,9 @@ import os
 
 
 def edge_detection(filepath): 
-
         img: Tensor = K.io.load_image(filepath, K.io.ImageLoadType.RGB32)
         img = img[None]
-
+        
         x_gray = K.color.rgb_to_grayscale(img)
         x_canny: Tensor = K.filters.canny(x_gray)[0]
         
@@ -22,17 +21,17 @@ def edge_detection(filepath):
         return output
     
 def save_canny(contentpath, savepath):
-    output= edge_detection(contentpath)
-    output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+        output= edge_detection(contentpath)
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
         
-    plt.imshow(output)
-    plt.axis('off')
-    savepath = os.path.join(savepath, 'canny.png')
-    plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
+        plt.imshow(output)
+        plt.axis('off')
+        savepath = os.path.join(savepath, 'canny.png')
+        plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
         
-    with Image.open(savepath) as im:
-        # Create a thumbnail of the image
-        im.thumbnail((512, 512))
-        # Save the thumbnail
-        im.save(savepath)
-    return savepath
+        with Image.open(savepath) as im:
+                # Create a thumbnail of the image
+                im.thumbnail((512, 512))
+                # Save the thumbnail
+                im.save(savepath)
+        return savepath
